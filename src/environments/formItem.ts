9d723f79507a45d5d7dbe3b3553a2732a418c551
WanchalermDevs <wanchalermj@nu.ac.th>
2018-10-31 15:25:03 +0700

    ปรับฟอร์ม หลัก

---
+++ src/environments/formItem.ts
@@ -0,0 +1,90 @@
+export const Income = [
+    { value: '1', viewValue: 'น้อยกว่า 150,000 บาทต่อปี' },
+    { value: '3', viewValue: 'ตั้งแต่ 150,000 - 300,000 บาทต่อปี' },
+    { value: '4', viewValue: 'ตั้งแต่ 300,000- 450,000 บาทต่อปี' },
+    { value: '5', viewValue: 'ตั้งแต่ 450,000 - 600,000 บาทต่อปี' },
+    { value: '6', viewValue: 'ตั้งแต่ 600,000 - 1,000,000 บาทต่อปี' },
+    { value: '6', viewValue: 'ตั้งแต่ 1,000,000 - 3,000,000 บาทต่อปี' },
+    { value: '6', viewValue: 'มากกว่า 3,000,000 บาทต่อปี' },
+];
+
+export const Career = [
+    { value: '1', viewValue: 'ข้าราชการ' },
+    { value: '2', viewValue: 'พนักงานหน่วยงานรัฐ' },
+    { value: '3', viewValue: 'พนักงานรัฐวิสาหกิจ' },
+    { value: '4', viewValue: 'นักธุรกิจ' },
+    { value: '5', viewValue: 'ค้าขาย' },
+    { value: '6', viewValue: 'เกษตรกร' },
+    { value: '7', viewValue: 'ไม่ได้ประกอบอาชีพ' },
+    { value: '8', viewValue: 'อื่นๆ' }
+];
+
+export const StudyStatusM1 = [
+    { value: '1', viewValue: 'กำลังศึกษาอยู่ชั้นประถมศึกษาปีที่ 6' },
+    { value: '2', viewValue: 'สำเร็จการศึกษาระดับชั้นประถมศึกษาปีที่ 6 แล้ว' },
+];
+
+export const StudyStatusM4 = [
+    { value: '1', viewValue: 'กำลังศึกษาอยู่ระดับชั้นมัธยมศึกษาปีที่ 3' },
+    { value: '2', viewValue: 'สำเร็จการศึกษาระดับชั้นมัธยมศึกษาปีที่ 3 แล้ว' },
+];
+
+export const CouseM1 = [
+    { value: '1', viewValue: 'เลือกแผนวิทยาศาสตร์ - คณิตศาสตร์ (ภาษาไทย)' },
+    { value: '2', viewValue: 'เลือกแผนวิทยาศาสตร์ - คณิตศาสตร์ (ภาษาอังกฤษ)' }
+];
+
+export const CouseM4 = [
+    { value: '1', viewValue: 'เลือกแผนวิทยาศาสตร์ - คณิตศาสตร์' },
+    { value: '2', viewValue: 'เลือกแผนภาษาอังกฤษ - คณิตศาสตร์' }
+];
+
+export const PreNmaes = [
+    { value: 'เด็กชาย', viewValue: 'เด็กชาย' },
+    { value: 'เด็กหญิง', viewValue: 'เด็กหญิง' },
+    { value: 'นาย', viewValue: 'นาย' },
+    { value: 'นางสาว', viewValue: 'นางสาว' }
+];
+
+export const NumOfDate = [
+    { value: '01', viewValue: '01' }, { value: '02', viewValue: '02' }, { value: '03', viewValue: '03' },
+    { value: '04', viewValue: '04' }, { value: '05', viewValue: '05' }, { value: '06', viewValue: '06' },
+    { value: '07', viewValue: '07' }, { value: '08', viewValue: '08' }, { value: '09', viewValue: '09' },
+    { value: '10', viewValue: '10' }, { value: '11', viewValue: '11' }, { value: '12', viewValue: '12' },
+    { value: '13', viewValue: '13' }, { value: '14', viewValue: '14' }, { value: '15', viewValue: '15' },
+    { value: '16', viewValue: '16' }, { value: '17', viewValue: '17' }, { value: '18', viewValue: '18' },
+    { value: '19', viewValue: '19' }, { value: '20', viewValue: '20' }, { value: '21', viewValue: '21' },
+    { value: '22', viewValue: '22' }, { value: '23', viewValue: '23' }, { value: '24', viewValue: '24' },
+    { value: '25', viewValue: '25' }, { value: '26', viewValue: '26' }, { value: '27', viewValue: '27' },
+    { value: '28', viewValue: '28' }, { value: '29', viewValue: '29' }, { value: '30', viewValue: '30' },
+    { value: '31', viewValue: '31' }
+];
+
+export const NumOfMounts = [
+    { value: '01', viewValue: 'มกราคม' },
+    { value: '02', viewValue: 'กุมภาพันธ์' },
+    { value: '03', viewValue: 'มีนาคม' },
+    { value: '04', viewValue: 'เมษายน' },
+    { value: '05', viewValue: 'พฤษภาคม' },
+    { value: '06', viewValue: 'มิถุนายน' },
+    { value: '07', viewValue: 'กรกฎาคม' },
+    { value: '08', viewValue: 'สิงหาคม' },
+    { value: '09', viewValue: 'กันยายน' },
+    { value: '10', viewValue: 'ตุลาคม' },
+    { value: '11', viewValue: 'พฤศจิกายน' },
+    { value: '12', viewValue: 'ธันวาคม' }
+];
+
+export const NumOfYears = [
+    { value: '2540', viewValue: '2540' },
+    { value: '2541', viewValue: '2541' },
+    { value: '2542', viewValue: '2542' },
+    { value: '2543', viewValue: '2543' },
+    { value: '2544', viewValue: '2544' },
+    { value: '2545', viewValue: '2545' },
+    { value: '2546', viewValue: '2546' },
+    { value: '2547', viewValue: '2547' },
+    { value: '2548', viewValue: '2548' },
+    { value: '2549', viewValue: '2549' },
+    { value: '2550', viewValue: '2550' }
+];
